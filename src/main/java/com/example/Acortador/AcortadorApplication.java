28228a92324b239e7714dedee99f1c6dd25ab5dc
Franco Lujan <francolujann1@gmail.com>
2025-06-19 17:29:58 -0300

    ESTRUCTURA Y CONFIG DE LA BASE DE DATOS

---
+++ src/main/java/com/example/Acortador/AcortadorApplication.java
@@ -0,0 +1,13 @@
+package com.example.Acortador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcortadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcortadorApplication.class, args);
+	}
+
+}
