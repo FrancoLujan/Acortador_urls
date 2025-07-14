e4d192fb9a5104b8ff52c7c9d32bd5be082a1374
Franco Lujan <francolujann1@gmail.com>
2025-07-14 12:11:32 -0300

    cambio de nombre de controlador a uno mas correcto

--- src/main/java/com/example/Acortador/controllers/AcortadorController.java
+++ src/main/java/com/example/Acortador/controllers/AcortadorController.java
@@ -3,27 +3,23 @@ package com.example.Acortador.controllers;
 import com.example.Acortador.DTOS.AcortarDTO;
 import com.example.Acortador.DTOS.Detalles.UrlDTODetalle;
 import com.example.Acortador.DTOS.UrlDTO;
-import com.example.Acortador.entities.Url;
 import com.example.Acortador.services.implementaciones.ServicioConvertidorImpl;
-import com.example.Acortador.services.implementaciones.UrlServicioImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URL;
 import java.util.List;
-import java.util.Objects;
 
 
 @RequestMapping("/api/acortador")
 @RestController
-public class UrlController {
+public class AcortadorController {
 
     private ServicioConvertidorImpl servicioConvertidor;
 
     @Autowired
-    public UrlController(ServicioConvertidorImpl servicioConvertidor) {
+    public AcortadorController(ServicioConvertidorImpl servicioConvertidor) {
         this.servicioConvertidor = servicioConvertidor;
     }
 
